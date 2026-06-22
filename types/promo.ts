abecc7596dff4820b08b01639997f8d01e687b6f
Brotherbond <ehinmitankehinde@gmail.com>
2026-06-22 09:20:30 +0100

    Updated Promo Details page. Added Promo title and Promo slabs

--- types/promo.ts
+++ types/promo.ts
@@ -2,8 +2,16 @@ export interface IPromo {
   id: string;
   uuid: string;
   type: string;
+  title?: string | null;
   start_date?: string | null;
   end_date?: string | null;
+  promo_slabs?: Array<{
+    uuid: string;
+    title: string;
+    bundle?: string | null;
+    reward?: string | null;
+    value?: string | number | null;
+  }>;
   created_at?: string;
   updated_at?: string;
 }
